17aee91ed7857b65d4814ef8cf3694fc9d70e1cf
Riccardo Persiani <r.persiani92@gmail.com>
2026-05-08 11:19:37 +0200

    Fix #513: add local interfaces (#534)

--- src/DssSpell.t.base.sol
+++ src/DssSpell.t.base.sol
@@ -165,7 +165,6 @@ interface SplitterMomLike {
     function stop() external;
 }
 
-// TODO: add full interfaces to dss-interfaces and remove from here
 interface FlapUniV2Like {
     function gem() external view returns (address);
     function pair() external view returns (address);
@@ -173,7 +172,6 @@ interface FlapUniV2Like {
     function want() external view returns (uint256);
 }
 
-// TODO: add full interfaces to dss-interfaces and remove from here
 interface SplitLike {
     function burn() external view returns (uint256);
     function farm() external view returns (address);
@@ -199,8 +197,13 @@ interface FlapOracleLike {
     function read() external view returns (bytes32);
 }
 
-// TODO: add full interfaces to dss-interfaces and remove from here
-interface UsdsJoinLike is DaiJoinAbstract {}
+interface UsdsJoinLike {
+    function dai() external view returns (address);
+    function exit(address usr, uint256 wad) external;
+    function join(address usr, uint256 wad) external;
+    function usds() external view returns (address);
+    function vat() external view returns (address);
+}
 
 interface SUsdsLike {
     function allowance(address, address) external view returns (uint256);
